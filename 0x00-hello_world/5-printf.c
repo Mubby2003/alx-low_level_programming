06ee53f9cc5a2c7f71a9575a6a5a6f2e31400e6c
Mubby2003 <adetonamubarak12@gmail.com>
2023-06-15 13:27:28 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- * 
- * The function will output a string
+ *
  * Return: Always 0 (Success)
- */
+ */	
 int main(void)
 {
-	char *a = "with proper grammar, but the outcome is a piece of art,";
-
-	printf("%s\n", a);
-
+	puts("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
