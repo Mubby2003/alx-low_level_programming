36c4e44ebfddd35df8857c02e6f56bcbcc574353
Mubby2003 <adetonamubarak12@gmail.com>
2023-06-15 13:36:05 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */	
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
