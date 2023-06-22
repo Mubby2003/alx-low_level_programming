7859852ce0b29a1de1e7f0565bdaeda2dd935399
Mubby2003 <adetonamubarak12@gmail.com>
2023-06-22 01:59:22 -0700

    mubby

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - tests function that prints if longer is postive or negative
+ * main - Test function for positive or negative
  * Return: 0
  */
-int main(void)
 
+int main(void)
 {
 	int i;
 
 	i = 0;
-	postive_or_negative(i);
+	positive_or_negative(i);
+
 	return (0);
 }
